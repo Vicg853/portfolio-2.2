3175d7a0728906f4151900242c84b43049e015d5
Vicg853 <vicg853@gmail.com>
2022-04-15 01:02:31 -0300

    Initial next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
+//const withPlugins = require('next-compose-plugins');
+
+const withLinaria = require('next-linaria');
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+  compiler: {
+    removeConsole: true,
+  },
+  //i18n: {
+//
+  //},
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  //experimental: {
+  //  
+  //},
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: '@svgr/webpack',
+      options: {
+        prettier: true,
+        svgo: true,
+        svgoConfig: { plugins: [{ removeViewBox: false }] },
+        titleProp: true,
+      },
+    });
+
+    return config;
+  },
 }
 
-module.exports = nextConfig
+module.exports = withLinaria(nextConfig)
