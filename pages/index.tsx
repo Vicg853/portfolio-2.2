fd54c7040ffbc536e9c04610ceb722a9e83fdfa4
Vicg853 <vicg853@gmail.com>
2022-04-16 03:22:56 -0300

    Simple header test on a test page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import type { NextPage } from 'next'
 
+import { Header } from '@components/header'
+
 const Home: NextPage = () => {
   return (
-    <div >
-      Hello World!
-    </div>
+    <>
+      <Header
+        title='Hi! I code dinosaurs 🦖'
+        description='I&apos;m Victor: a computer science and whole-stack lover, &#10; but full-stack focused for the moment.'
+        background={{
+          type: 'image',
+          srcType: 'local',
+          src: '/images/pages/jacky-lo-LjX-m_UXQGM-unsplash.jpg',
+        }}
+       />
+      <div style={{ height: '100vh' }}>
+        Hello World!
+      </div>
+    </>
   )
 }
 
