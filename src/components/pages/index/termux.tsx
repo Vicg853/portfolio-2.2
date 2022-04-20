359c8ece76a0774ff6302dce1b064f60eeafafa3
Vicg853 <vicg853@gmail.com>
2022-04-20 16:30:19 -0300

    Saving termux element progress

---
+++ src/components/pages/index/termux.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+
+const Term = () => {
+   //useEffect(() => {
+   //   const xterm = (await import('xterm'))
+//
+   //   const term = new xterm()
+//
+   //   const term = document.getElementById("term")
+   //}
+   return (
+      <div style={{ height: '200vh'}}>
+         Term
+      </div>
+   )
+}
+
+export default Term
