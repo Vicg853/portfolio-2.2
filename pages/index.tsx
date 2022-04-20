0357b5793fe5a4f7518568285063da208d55137e
Vicg853 <vicg853@gmail.com>
2022-04-20 16:30:06 -0300

    Adding main termux layout and dynamic import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,56 @@
 import type { NextPage } from 'next'
+import React, { Suspense } from 'react'
+import dynamic from 'next/dynamic'
 
 import { Header } from '@components/header'
 
+//* Importing style elements
+import {
+  Container,
+  Section,
+  terminalContainer
+} from '@p-styles/index'
+
+const LoadingTermComponent = () => (
+	<span id='loading-term' style={{height: '200vh', display: 'block'}}>
+		{"❯"} shell <br/>
+		starting terminal and loading dinosaurs 🦖<br/>
+	</span>
+)
+
+const TermuxTerminal = dynamic(() => import('@components/pages/index/termux'), {
+	ssr: false,
+	suspense: true,
+	loading: LoadingTermComponent
+})
+
 const Home: NextPage = () => {
-  return (
-    <>
-      <Header
-        title='Hi! I code dinosaurs 🦖'
-        description='I&apos;m Victor: a computer science and whole-stack lover, &#10; but full-stack focused for the moment.'
-        background={{
-          type: 'image',
-          srcType: 'local',
-          src: '/images/pages/index/LRM_20200528_151756 (1).jpg',
-        }}
-       />
-      <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        Hello World! <br />
-        This website is still under construction 🚧🏗️
-      </div>
-    </>
-  )
+  	return (
+  	  	<>
+  	  		<Header title='Hi! I code dinosaurs 🦖'
+  	  		  	description='I&apos;m Victor: a computer science and whole-stack lover, &#10; but full-stack focused for the moment.'
+  	  		  	background={{
+  	  		  	  type: 'image',
+  	  		  	  srcType: 'local',
+  	  		  	  src: '/images/pages/index/LRM_20200528_151756 (1).jpg',
+  	  		  	}}
+  	  		 />
+  	  		 <Container>
+  	  			<Section className={terminalContainer}>
+					<sub id='term-bar'>
+						<span className='red-button' />
+						<span className='grey-button' />
+						<span className='green-button' />
+					</sub>
+					<sub id='term-content'>
+						<Suspense fallback={<LoadingTermComponent/>}>
+							<TermuxTerminal />
+						</Suspense>
+					</sub>
+  	  			</Section>
+  	  		 </Container>
+  	  	</>
+  	)
 }
 
 export default Home
---
+++ src/styles/index.ts
@@ -0,0 +1,116 @@
+import { styled } from 'linaria/react'
+import { css } from 'linaria'
+ 
+export const Container = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: flex-start;
+   align-items: stretch;
+   width: 100vw;
+   padding: 3rem 5vw;
+   height: auto;
+`
+
+export const Section = styled.section`
+   display: inline-flex;
+   justify-content: space-around;
+   align-items: center;
+   flex-wrap: wrap;
+`
+
+export const terminalContainer  = css`
+   --term-bar-height: 25px;
+
+   width: 100%;
+   max-width: 700px;
+   height: 400px;
+   overflow: hidden;
+   
+   display: flex;
+   flex-direction: column;
+   align-items: stretch;
+
+   #term-bar {
+      display: inline-flex;
+      justify-content: flex-start;
+      height: var(--term-bar-height);
+      align-items: center;
+      gap: 4px;
+      background-color: var(--pallete-bgContrast);
+      border-radius: 0.35rem;
+      padding: 0rem 1rem;
+      span {
+         --button-size: 7px;
+         border-radius: 50%;
+         width: var(--button-size);
+         height: var(--button-size);
+      }
+      .red-button {
+         background: var(--pallete-constants-err);
+      }
+      .green-button {
+         background: var(--pallete-constants-success);
+      }
+      .grey-button {
+         background: var(--pallete-opaque-bg);
+      }
+   }
+   #term-content {
+      height: calc(100% - var(--term-bar-height));
+      padding: 0.7rem 1rem;
+      overflow-y: scroll;
+      border-bottom-left-radius: 0.4rem;
+      border-bottom-right-radius: 0.4rem;
+      border: 1.5px solid var(--pallete-bgContrast);
+      border-top: none;
+      font-family: var(--fonts-tertiary);
+      font-weight: 500;
+      font-size: 0.7rem;
+
+      ::-webkit-scrollbar {
+         width: 1rem;
+      }
+      ::-webkit-scrollbar-thumb {
+         background: var(--pallete-bgContrast);
+         background-clip: padding-box;
+         border: 0.43rem solid transparent;
+         border-radius: 1rem;
+         :hover {
+            background: var(--pallete-opaque-accent);
+            background-clip: padding-box;
+         }
+      }
+      ::-webkit-scrollbar-track {
+         background: transparent;
+      }
+
+      #loading-term {
+         display: block;
+         align-self: center;
+         justify-self: center;
+         color: var(--pallete-text);
+         animation: name duration timing-function delay iteration-count direction fill-mode;
+         ::after {
+            content: '.';
+            animation-name: termLoadDotsAnimation;
+            animation-duration: 3s;
+            animation-iteration-count: infinite;
+            
+            @keyframes termLoadDotsAnimation {
+               0% {
+                  content: '';
+               }
+               35% {
+                  content: '.';
+               }
+               65% {
+                  content: '..';
+               }
+               100% {
+                  content: '...';
+               }
+            }
+         }
+      }
+   }
+`
