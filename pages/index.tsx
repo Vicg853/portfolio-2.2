3b36682dfd665fa14f5940d046494e1a25ec8ba1
Vicg853 <vicg853@gmail.com>
2022-04-25 23:00:53 -0300

    Adding initial temporary objectives function

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,26 +37,95 @@ const TermuxTerminal = dynamic(() => import('@components/pages/index/xterm'), {
 	ssr: false,
 })
 
+
+interface GlobalObjectivesSourceType {
+	objectiveProgress: 'to-do' | 'in-progress' | 'done'
+	objectiveId: string
+	objectiveFeats?: string[]
+	objectiveSources?: string[]
+	objectiveProjects?: string[]
+}
+
+interface ObjectivesSourcePageProps extends GlobalObjectivesSourceType {
+	objectiveName: string,
+	objectiveDescription: string,
+}
+
+interface ObjectivesSourceQueryType extends GlobalObjectivesSourceType {
+	sourceI18nIterations: Record<'en' | 'pt' | 'fr', {
+		objectiveName: string
+		objectiveDescription: string
+	}>
+}
+
 export interface IndexPageLocaleContent {
 	mainP: string[]
 	imageCaption: string
 	terminal?: any
 }
 
+type PageProps =  PageFullType<IndexPageLocaleContent> & {
+	objectives: ObjectivesSourcePageProps[]
+}
+
 export const getStaticProps: GetStaticProps<{
 	locale: string
-	pageSource: PageFullType<IndexPageLocaleContent>
+	pageSource: PageProps
 }> = async ({ locale, locales }) => {
 	const pageSource = getPageSource(locale, 'index')
+
+	//TODO Add real graphql source when apollo is ready
+	const objectivesSource: ObjectivesSourceQueryType[] = [
+		{
+			objectiveId: 'objective-learn-rust-lang',
+			objectiveProgress: 'in-progress',
+			sourceI18nIterations: {
+				'en': {
+					objectiveName: 'Learn Rust',
+					objectiveDescription: 'Learn the Rust programming language.'
+				},
+				'pt': {
+					objectiveName: 'Aprender Rust',
+					objectiveDescription: 'Aprender a linguagem de programação Rust .'
+				},
+				'fr': {
+					objectiveName: 'Apprendre Rust',
+					objectiveDescription: 'Apprendre le langage de programmation Rust.'
+				}
+			},
+			objectiveSources: [
+				'https://rust-lang.org'
+			]
+		}
+	]
+	
+	//filter out only the current locale name and descriptions
+	const objectives: ObjectivesSourcePageProps[] = objectivesSource.map(objective => {
+		const objectiveLocale = objective.sourceI18nIterations[(locale ?? defaultLocale) as 'en' | 'pt' | 'fr'] ??
+			objective.sourceI18nIterations[defaultLocale]
+
+		return {
+			...objective,
+			sourceI18nIterations: null,
+			objectiveName: objectiveLocale.objectiveName,
+			objectiveDescription: objectiveLocale.objectiveDescription,
+		}
+	})
+
+	
+
 	return {
 		props: {
-			pageSource,
+			pageSource: {
+				...pageSource,
+				objectives
+			},
 			locale: locale ?? locales![0],
 		},
 	}
 }
 
-const Home: NextPage<{pageSource: PageFullType<IndexPageLocaleContent>}> = ({ pageSource  }) => {
+const Home: NextPage<{pageSource: PageProps}> = ({ pageSource  }) => {
   	return (
   	  	<>
   	  		<Header title={pageSource.mainProps.header!.title}
