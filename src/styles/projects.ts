171e73e91cc5fc657c0a9fd617cedbe3bf4726a8
Vicg853 <vicg853@gmail.com>
2022-04-27 19:35:40 -0300

    Saving current progress

---
+++ src/styles/projects.ts
@@ -0,0 +1,14 @@
+import { css } from 'linaria'
+import { styled } from 'linaria/react'
+
+export const calloutAlignment = css`
+   @media (min-width: 900px) {
+      position: absolute;
+      top: calc(100vh + 1rem);
+      left: 5vw;
+   }
+`
+
+export const introductionSizing = css`
+   width: 600px;
+`
