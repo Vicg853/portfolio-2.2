28053a8bba3aba1fce835abdbc7dc52cee30e32c
Vicg853 <vicg853@gmail.com>
2022-04-30 18:22:45 -0300

    Setting up contact page locales

---
+++ src/locales/fr/page/contact.ts
@@ -0,0 +1,9 @@
+import type { PageFullType } from '../../index'
+import type { ContactPageLocaleContent } from '@pages/contact'
+
+export const ProjectsPage: PageFullType<ContactPageLocaleContent> = {
+   mainProps: {},
+   content: {
+      
+   },
+}
