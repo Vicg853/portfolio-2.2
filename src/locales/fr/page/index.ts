5e96300703f24778aeccdcc58405fa76018d3f7b
Vicg853 <vicg853@gmail.com>
2022-05-04 01:11:17 -0300

    Adding a title to captions on index page and trying to improve written content a bit

--- src/locales/fr/page/index.ts
+++ src/locales/fr/page/index.ts
@@ -3,15 +3,16 @@ import type { IndexPageLocaleContent } from '@pages/index'
 
 export const IndexPage: PageFullType<IndexPageLocaleContent> = {
    content: {
-      mainP: 'Je suis passionné par l\'informatique et le whole-stack  et cela ne s\'agit pas d\’une métaphore! Cela me mène à en apprendre toujours plus sur le sujet, du plus théorique aux plus pratique. \n\nDès l\'âge de 12 ans j\'explore ce sujet. J\'ai donc beaucoup appris sur ce monde et dès cette introduction j\’ai pu apprendre que ce monde est en constante évolution,  une des raisons qui justifient ma passion: évolution constante === infinité possibilités.\n\nSur ce site web vous pourrez découvrir ce que j\'apprends et vais encore apprendre, accompagné d\’une variété de projets de différentes portées: open-source, IoT, Hardware, Sites Web, Application, projets de technologie au sein de NGOs, ...',
-      imageCaption: 'Moi!',
+      mainP: 'Je suis passionné par l\'informatique et le "whole-stack", un chercheur enthousiaste du sujet. \n\nDès l\'âge de 12 ans je m\'approfondi dans le sujet et dès cette introduction j\’ai pu apprendre que ce sujet est en constante croissance, ce qui justifie encore plus ma passion.\n\nSur ce site web vous pourrez découvrir ce que je sais, ainsi que mes curiosités et projets (en cours et futurs) de portées open-source, IoT, Hardware, Sites Web, Application, projets de technologie au sein de NGOs, etc.',
+      imageCaption: 'Salut!',
       objectivesText: {
          title: 'Paramètres\n toujours en jour &\n en route',
          description: 'a.k.a.: mes objectifs pour cette année',
-         objectivesLegend: {
-            done: 'Conclu 🎉',
-            inProgress: 'En progrès...',
-            todo: 'En pause / non commencé',
+         objectivesCaption: {
+            title: 'Legende',
+            done: 'Projets conclus 🎉',
+            inProgress: 'Projets en cours',
+            todo: 'Projets à venir',
          }
       }
    },
