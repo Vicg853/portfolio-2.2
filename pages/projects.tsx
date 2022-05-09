4ae04f8f1b87a90db7089a06bd7625fdfe97c7eb
Vicg853 <vicg853@gmail.com>
2022-05-09 05:03:05 -0300

    Recreating projects page and adding dummy content

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,16 @@
 import type { NextPage, GetStaticProps } from 'next'
 import type { PageFullType } from '../src/locales'
+import type { ProjectsSource, ProjectsListType } from '@api-utils/content-retrivers/projects'
 
 //* Importing needed components and deps
 import { Header } from '@components/header'
+import Image from 'next/image'
+import Link from 'next/link'
+import { cx } from 'linaria'
 
 //* Importing api components and deps
 import { getPageSource } from '@api-utils/locales-sources'
+import { getProjectsList } from '@api-utils/content-retrivers/projects'
 
 //* Importing global and specific styled components
 import { 
@@ -16,41 +21,65 @@ import {
 	TextEffect
 } from '@p-styles/global'
 import {
-	containerMargins
+	ProjectCard,
+	ProjectCardMiniCard,
+	containerMargins,
+	projectCardTitleStyles,
+	accessProjectLinksStyle
 } from '@p-styles/projects'
 
-//* Importing used components
-import { InTextLink } from '@components/mini-components/InTextLink'
+//* Importing used assets
+import GithubIcon from '@p-images/projects/github-source-icon-icons8.svg'
+import WWWIcon from '@p-images/projects/www-source-icon-icons8.svg'
+import MoreInfIcon from '@p-images/projects/moreInf-source-icon-icons8.svg'
+import RelatedIcon from '@p-images/projects/related-sources-icon-icons8.svg'
 
 export interface ProjectsPageStaticLocalesSource {
 	mainParagraphTitle: string
    mainParagraph: string
-
+	projectsList: {
+		sources: {
+			github: string
+			www: string
+			moreInf: string
+			related: string
+		}
+		techStack: string
+		scope: { plural: string, singular: string }
+		topics: { plural: string, singular: string }
+		access: string
+	}
 }
 
 type ProjectsPageSource = PageFullType<ProjectsPageStaticLocalesSource>
 
-export const getStaticProps: GetStaticProps<{
-	locale: string
+interface GetStaticPropsResult {
+	projectsList: ProjectsListType
 	pageSource: ProjectsPageSource
-}> = async ({ locale, locales }) => {
+	locale: string
+}
+
+export const getStaticProps: GetStaticProps<GetStaticPropsResult> = async ({ locale, locales }) => {
 	const pageSource = getPageSource(locale, 'projects')
 	
+	const projectsList = await getProjectsList(locale as any, locales![0] as any)
 
 	return {
 		props: {
 			pageSource: {
-				...pageSource
+				...pageSource,
 			},
+			projectsList,
 			locale: locale ?? locales![0],
 		},
 	}
 }
 
-const ProjectsComponent: NextPage<{pageSource: ProjectsPageSource, locale: string}> = ({ pageSource, locale }) => {
+const ProjectsComponent: NextPage<GetStaticPropsResult> = ({ pageSource, locale, projectsList }) => {
 	const {
 		mainParagraphTitle,
-		mainParagraph
+		mainParagraph,
+		projectsList: projectsListCaptions
 	} = pageSource.content
 
    return (
@@ -67,7 +96,7 @@ const ProjectsComponent: NextPage<{pageSource: ProjectsPageSource, locale: strin
          />
          <Container className={containerMargins}>
 				<Section data-vert data-gap
-				data-widthMax data-limitWidthBig>
+				data-widthMax>
 					<SecTitle>
 						<TextEffect data-codeBigArrow>
 							{mainParagraphTitle}
@@ -77,6 +106,96 @@ const ProjectsComponent: NextPage<{pageSource: ProjectsPageSource, locale: strin
 						{mainParagraph.split(/\n/).map(val => <>{val}<br key={val}/></>)}
 					</Paragraph>
 				</Section>
+				{projectsList.map((project, i) => {
+					const {
+						frontmatter,
+						metadata,
+						sources
+					} = project
+
+					return (
+						<Section data-widthMax data-vert key={i}> 
+							<ProjectCard>
+								<Image className='project-card-image' layout='fill' objectFit='cover'
+								src={frontmatter.image} alt={`${frontmatter.title} project background illustration!`}/>
+								<sub>
+									<h3 className={cx(
+										SecTitle.__linaria.className, 
+										projectCardTitleStyles)}>
+										{frontmatter.title}
+									</h3>
+									<span className='project-card-project-description'>
+										{frontmatter.description}
+									</span>
+									{metadata.scopes && <span className='project-card-project-scope'>
+										{metadata.scopes.length > 1 ? 
+											projectsListCaptions.scope.plural
+											: projectsListCaptions.scope.singular 
+										}{":"}
+										<span className='detail'>{metadata.scopes.join(',')}</span>
+									</span>}
+									<span className='project-card-section-title'>
+										{metadata.topics.length > 1 ? 
+											projectsListCaptions.topics.plural 
+											: projectsListCaptions.topics.singular 
+										}{":"}
+									</span>
+									<section className='project-card-project-subjects'>
+										{metadata.topics.map((topic, i) => (
+											<ProjectCardMiniCard key={i}>{topic}</ProjectCardMiniCard>
+										))}
+									</section>
+									<span className='project-card-section-title'>{`${projectsListCaptions.techStack}:`}</span>
+									<section className='project-card-project-technologies'>
+										{metadata.techStack.map((tech, i) => (
+											<ProjectCardMiniCard key={i}>
+												{tech.techLink ?
+													<Link href={tech.techLink} passHref>
+														<a>{tech.techName}</a>
+													</Link>
+												: tech.techName}
+											</ProjectCardMiniCard>
+										))}
+									</section>
+									<section className='project-card-project-access'>
+										<span className='title'>{projectsListCaptions.access}</span>
+										<div className='content'>
+											{sources.map((source, i) => (
+												<Link passHref href={source.sourceLink} key={i}>
+													<a className={accessProjectLinksStyle} 
+            		      					   rel='noopener noreferrer'
+            		      					   target='_blank'>
+														{source.sourceType === 'GITHUB' && (<>
+																<GithubIcon class='icon' />
+																{projectsListCaptions.sources.github}
+														</>)}
+														{source.sourceType === 'WEBSITE' && (<>
+																<WWWIcon class='icon' />
+																{projectsListCaptions.sources.www}
+														</>)}
+														{source.sourceType === 'MOREINFO' && (<>
+																<MoreInfIcon class='icon' />
+																{projectsListCaptions.sources.moreInf}
+														</>)}
+														{source.sourceType === 'RELATED' && (<>
+																<RelatedIcon class='icon' />
+																{projectsListCaptions.sources.related}
+														</>)}
+														{source.sourceType === 'CUSTOM' && (<>
+															<Image className='icon' layout='fill'
+															src={source.sourceIcon} alt={source.sourceText} />
+															{source.sourceText}
+														</>)}
+            		      					</a>
+												</Link>
+											))}
+										</div>
+									</section>
+								</sub>
+							</ProjectCard>
+						</Section>
+					)
+				})}
          </Container>
       </>
    )
--- src/styles/projects.ts
+++ src/styles/projects.ts
@@ -17,3 +17,168 @@ export const containerMargins = css`
    margin-top: 4rem;
    margin-bottom: 4rem;
 `
+
+export const ProjectCard = styled.div`
+   position: relative;
+   display: inline-flex;
+   align-items: center;
+   justify-content: flex-start;
+   
+   width: 100%;
+   height: 600px;
+   
+   border-radius: 0.6rem;
+   overflow: hidden;
+
+   border: 1px solid var(--pallete-bgAlt);
+
+   .project-card-image {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+   }
+
+   --sub-card-margin: 1rem;
+   sub {
+      z-index: 1;
+      width: 600px;
+      max-width: calc(100% - var(--sub-card-margin) * 2);
+      min-height: calc(100% - 2 * var(--sub-card-margin));
+      margin: var(--sub-card-margin);
+
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: stretch;
+      gap: 1rem;
+      padding: 1.3rem 0.9rem;
+
+      border-radius: 0.6rem;
+      overflow: hidden;
+      
+      background: var(--pallete-opaque-bg);
+      backdrop-filter: blur(1rem);
+
+      border: 1px solid var(--pallete-bgAlt);
+   }
+
+   .project-card-project-description,
+   .project-card-project-scope,
+   .project-card-section-title,
+   .project-card-project-access > .title {
+      font-size: 0.85rem;
+      font-weight: 400;
+      font-family: var(--fonts-primary);
+      color: var(--pallete-text);
+   }
+
+   .project-card-project-scope {
+      display: inline-flex;
+      justify-content: flex-start;
+      align-items: center;
+      margin-top: 15%;
+   }
+   .project-card-project-scope > .detail {
+      color: var(--pallete-accent);
+      margin-left: 0.5rem;
+   }
+
+   .project-card-project-subjects,
+   .project-card-project-technologies {
+      display: inline-flex;
+      justify-content: flex-start;
+      align-items: flex-start;
+      flex-wrap: wrap;
+      gap: 0.2rem;  
+      max-height: 64px;
+      overflow-y: scroll;
+   }
+
+   .project-card-project-access {
+      display: flex;
+      flex-direction: column;
+
+      background: var(--pallete-opaque-bgContrast);
+      border-radius: 0.6rem;
+      padding: 0.8rem 1.1rem;
+      margin-top: auto;
+   }
+   .project-card-project-access > .title {
+      font-size: 0.9rem;
+   }
+   .project-card-project-access > .content {
+      display: inline-flex;
+      justify-content: flex-start;
+      align-items: center;
+      gap: 0.8rem;
+      margin-top: 0.8rem;
+      flex-wrap: wrap;
+   }
+
+   @media (max-width: 700px) {
+      border: none;
+   }
+`
+
+export const projectCardTitleStyles = css`
+   font-size: 1rem;
+   font-weight: 700;
+   ::after {
+      margin-top: 0.5rem;
+      width: 95%;
+   }
+`
+
+export const ProjectCardMiniCard = styled.div`
+   font-size: 0.85rem;
+   font-weight: 400;
+   font-family: var(--fonts-secondary);
+   color: var(--pallete-text);
+   width: auto;
+   
+   background-color: var(--pallete-bg);
+   border-radius: 0.3rem;
+   border: 1px solid var(--pallete-opaque-bgAlt);
+
+   padding: 0.25rem 0.45rem;
+
+   a {
+      text-decoration: underline;
+      text-decoration-color: var(--pallete-accent);
+      cursor: pointer;
+      font-size: 0.85rem;
+      font-weight: 400;
+      font-family: var(--fonts-secondary);
+      color: var(--pallete-text);
+   }
+`
+
+export const accessProjectLinksStyle = css`
+   background-color: var(--pallete-bg);
+   color: var(--pallete-text);
+   border-radius: 0.6rem;
+   border: 1px solid transparent;
+   padding: 0.5rem 0.8rem;
+   font-size: 0.8rem;
+   font-weight: 500;
+   font-family: var(--fonts-secondary);
+   transition-property: color, border-color;
+   transition-duration: 0.3s;
+   transition-timing-function: ease-in-out;
+   cursor: pointer;
+
+   display: inline-flex;
+   align-items: center;
+   justify-content: center;
+   gap: 1rem;
+
+   .icon {
+      width: 1.2rem;
+      height: 1.2rem;
+   }
+
+   &:hover {
+      border: 1px solid var(--pallete-accent);
+      color: var(--pallete-accent);
+   }
+`
