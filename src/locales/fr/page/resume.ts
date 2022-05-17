241eb554d4fa590625cdbe32be1e58d81ad325fb
Vicg853 <vicg853@gmail.com>
2022-05-16 23:47:33 -0300

    Removing fr source for the moment

--- src/locales/fr/page/resume.ts
+++
@@ -1,8 +0,0 @@
-import type { PageFullType } from '../../index'
-import type { ResumePageLocaleContent } from '@pages/resume'
-
-export const ResumePage: PageFullType<ResumePageLocaleContent> = {
-   content: {
-
-   }
-}
