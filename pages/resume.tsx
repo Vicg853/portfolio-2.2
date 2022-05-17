49497baffe3d630d7719ac3665429ec3817444bc
Vicg853 <vicg853@gmail.com>
2022-05-16 23:47:10 -0300

    Adding locale sourcing to resume page

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -30,7 +30,30 @@ import {
 
 
 export interface ResumePageLocaleContent {
-	
+   mainParagraph: string
+   contactMePageLink: string
+   contactMePageAlt: string
+   cvSections: {
+      skills: {
+         title: string
+         categoryFilter: string
+         categoryAllOption: string
+         searchFilter: string
+         techExpYears: string
+         projectsNumber: string
+         skillSource: string
+         relatedProjects: string
+      }
+      experience: string
+      education: string
+   }
+   index: {
+      title: string
+      skills: string
+      experience: string
+      education: string
+      cvLink: string
+   }
 }
 
 interface PageProps {
@@ -62,31 +85,33 @@ const scrollTo = (id: string) => {
       el.scrollIntoView({ behavior: 'smooth', block: 'center' })
 }
 
-const RawSkillsContainer = ({ skills }: {skills: CVPageCMSContent['skills']}) => {
+const RawSkillsContainer = ({ 
+      skills,
+      captionsLocaleSources
+   }: { 
+      skills: CVPageCMSContent['skills'], 
+      captionsLocaleSources: ResumePageLocaleContent['cvSections']['skills']
+   }) => {
    const [techSearch, setTechSearch] = useState<string>('')
    const [selectedCategory, setSelectedCategory] = useState<SkillCategoryType | 'all'>('all')
 
    return (
-      <Section data-widthMax
-      data-vert className={filterSkillsSectionStyle}>
-         <SecTitle id='skills-section'>
-            #skills
-         </SecTitle>
+      <>
          <Section id="filters-sections-container">
             <Section className='filters-sections' data-vert data-gap>
-               <span>Category:</span>
+               <span>{`${captionsLocaleSources.categoryFilter}:`}</span>
                <select className={Input.__linaria.className} onChangeCapture={e => 
                      setSelectedCategory(e.currentTarget
                         .value as SkillCategoryType)
                   }>
-                  <option value='all'>All</option>
+                  <option value='all'>{`${captionsLocaleSources.categoryAllOption}`}</option>
                   {skills.categories.map(category => (
                      <option key={category} value={category}>{category}</option>
                   ))}
                </select>
             </Section>
             <Section className='filters-sections' data-vert data-gap>
-               <span>Skill:</span>
+               <span>{`${captionsLocaleSources.searchFilter}:`}</span>
                <Input onChange={e => 
                   setTechSearch(e.currentTarget.value)
                } placeholder={`e.g.: NextJs`} />
@@ -100,15 +125,21 @@ const RawSkillsContainer = ({ skills }: {skills: CVPageCMSContent['skills']}) =>
                      key={skill.skill}>
                      <sub>
                         <h5>{skill.skill}</h5>
-                        <span><span className='detail'>Experience years:</span> {skill.experienceYears}</span>
-                        <span><span className='detail'>Projects:</span> {skill.projects}</span>
+                        <span>
+                           <span className='detail'>{`${captionsLocaleSources.techExpYears}:`}</span> 
+                           {skill.experienceYears}
+                        </span>
+                        <span>
+                           <span className='detail'>{`${captionsLocaleSources.projectsNumber}:`}</span> 
+                           {skill.projects}
+                        </span>
                      </sub>
                      {(skill.relatedProjectList || skill.skillSource) && (
                         <sub className='mini-cards-sub'>
                            {skill.relatedProjectList && (
                               <button onClick={() => {}}
                               className='skill-projects'>
-                                 Projects
+                                 {`${captionsLocaleSources.relatedProjects}:`}
                               </button>
                            )}
                            {skill.skillSource && (
@@ -116,7 +147,7 @@ const RawSkillsContainer = ({ skills }: {skills: CVPageCMSContent['skills']}) =>
                               target='_blank' 
                               rel='noopener noreferrer'
                               className='skill-source'>
-                                 Skill source
+                                 {`${captionsLocaleSources.skillSource}:`}
                               </a>
                            )}
                         </sub>
@@ -125,17 +156,25 @@ const RawSkillsContainer = ({ skills }: {skills: CVPageCMSContent['skills']}) =>
                )
             })}
          </Section>
-      </Section>
+      </>
    )
 }
 
 const SkillsContainer = memo(RawSkillsContainer, () => true)
 
-const Resume: NextPage<PageProps> = ({ locale, skills }) => {
+const Resume: NextPage<PageProps> = ({ locale, skills, pageSource }) => {
    useEffect(() => {
       if(document.location.hash !== '') 
          return scrollTo(`${document.location.hash.replace('#', '')}-section`)
    })
+
+   const {
+      mainParagraph,
+      contactMePageLink,
+      contactMePageAlt,
+      cvSections,
+      index
+   } = pageSource.content
 
    return ( 
       <>
@@ -155,47 +194,53 @@ const Resume: NextPage<PageProps> = ({ locale, skills }) => {
                className={introParagraphStyles}>
                   <SecTitle>Curriculum<br/>Vitae</SecTitle>
                   <Paragraph data-limitWidthMed>
-                     On this page, you will find a list of my experiences, knowledge, skills, and a direct link to my CV. 
-                     <br/><br/>
-                     As knowledge is sometimes harder to picture than just describing projects, I’m taking the following approach (which I think is the best way to stream it): each technology skill card will have a year/month based number of continuous usage, and the number of projects on which I used it.
-                     <br/><br/>
-                     Although if you need clarification or any tips for a different approach, feel free to contact me 
-                     <InTextLink href='/contact' locale={locale}>here</InTextLink> or via email:
+                     {mainParagraph.split('\n').map((val, i) => (<>{val}<br key={i}/></>))}
+                     <InTextLink href='/contact' locale={locale}>{contactMePageLink}</InTextLink> 
+                     {contactMePageAlt}
                      <InTextLink href='mailto:vicg853@gmail.com'>vicg853@gmail.com</InTextLink>.
                   </Paragraph>
                </Section>
                <IndexCard>
-                  <SecTitle>Index</SecTitle>
+                  <SecTitle>{index.title}</SecTitle>
                   <span> 
                      - 
                      <InTextLink 
                      href='/resume#skills'>
-                        Skills
+                        {index.skills}
                      </InTextLink>
                   </span>
                   <span> 
                      - 
                      <InTextLink 
                      href='/resume#experience'>
-                        Experience
+                        {index.experience}
                      </InTextLink> 
                   </span>
                   <span>
                       - 
                       <InTextLink 
                       href='/resume#education'>
-                         Education
+                         {index.education}
                      </InTextLink> 
                   </span>
                   <span>
                       - 
                       <InTextLink href='/curriculum-vitae.pdf'>
-                         Full CV PDF
+                         {index.cvLink}
                      </InTextLink> 
                   </span>
                </IndexCard>
             </Section>
-            <SkillsContainer skills={skills} />
+            <Section data-widthMax
+            data-vert className={filterSkillsSectionStyle}>
+               <SecTitle id='skills-section'>
+                  {`#${cvSections.skills.title}`}
+               </SecTitle>
+               <SkillsContainer 
+                  skills={skills} 
+                  captionsLocaleSources={cvSections.skills} 
+                  />
+            </Section>
          </Container>
       </>
    )
--- src/locales/en-US/page/resume.ts
+++ src/locales/en-US/page/resume.ts
@@ -3,6 +3,29 @@ import type { ResumePageLocaleContent } from '@pages/resume'
 
 export const ResumePage: PageFullType<ResumePageLocaleContent> = {
    content: {
-
+      mainParagraph: 'On this page, you will find a list of my experiences, knowledge, skills, and a direct link to my CV.\n\nAs knowledge is sometimes harder to picture than just describing projects, I’m taking the following approach (which I think is the best way to stream it): each technology skill card has its years/month of usage/research and the number of projects using it.\n\nAlthough if you need clarification or any tips for a different approach, feel free to contact me',
+      contactMePageAlt: 'or via email at',
+      contactMePageLink: 'here',
+      index: {
+         title: 'Summary',
+         education: 'My Education',
+         experience: 'My Experience',
+         skills: 'My Skills',
+         cvLink: 'Download/See my CV in PDF',
+      },
+      cvSections: {
+         education: 'Education',
+         experience: 'Experience',
+         skills: {
+            title: 'Skills',
+            categoryAllOption: 'All',
+            categoryFilter: 'Filter by category',
+            projectsNumber: 'Projects',
+            skillSource: 'Source',
+            relatedProjects: 'Related Projects',
+            searchFilter: 'Search for:',
+            techExpYears: 'Years of experience',
+         },
+      }
    }
 }
