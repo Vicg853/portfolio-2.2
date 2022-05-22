bae724b43a2a358d32710290f32c5049e87cbc76
Vicg853 <vicg853@gmail.com>
2022-05-22 01:54:46 -0300

    Improving resume page locale

--- src/locales/pt-BR/page/resume.ts
+++ src/locales/pt-BR/page/resume.ts
@@ -10,15 +10,15 @@ export const ResumePage: PageFullType<ResumePageLocaleContent> = {
       }
    },
    content: {
-      mainParagraph: 'Nesta página, você irá encontrar uma lista com minhas experiências, conhecimento, habilidades e um link de acesso com o meu CV.\n\nComo conhecimento algumas vezes pode ser algo meio desafiador de ilustrar, adotei a seguinte abordagem (que penso ser o melhor jeito de transmitir): cada card de tecnologia/skill terá um nível de experiência baseado em anos/meses e o número de projetos na qual está participou.\n\Caso queira esclarecimento e/ou tenha qualquer sugestão de uma abordagem diferente, sinta se à vontade para mandar uma mensagem',
+      mainParagraph: 'Descubra aqui mais sobre minhas experiências, conhecimento e competências. Você também pode visualizar meu C.V. anexado ao lado.\n\nCaso precise de mais informação ou esclarecimentos, sinta se avontade para entrar em contato',
       contactMePageAlt: 'ou mandar um email em',
       contactMePageLink: 'aqui',
       index: {
          title: 'Sumário',
-         education: 'Minha Educação',
-         experience: 'Minhas Experiências',
-         skills: 'Minhas Habilidades',
-         cvLink: 'Baixar/Ver meu CV em PDF',
+         education: 'Educação',
+         experience: 'Experiências',
+         skills: 'Competências',
+         cvLink: 'Baixar/visualizar meu CV em PDF',
       },
       cvSections: {
          education: {
@@ -34,7 +34,7 @@ export const ResumePage: PageFullType<ResumePageLocaleContent> = {
             sinceCaption: 'Desde:',
          },
          skills: {
-            title: 'Habilidades',
+            title: 'Competências',
             categoryAllOption: 'Todas',
             categoryFilter: 'Filtrar por categoria',
             projectsNumber: 'Projetos',
