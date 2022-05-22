98f895adab928713b647221a3f509247affbeac3
Vicg853 <vicg853@gmail.com>
2022-05-22 01:13:43 -0300

    Imrpoving projects page content

--- src/locales/pt-BR/page/projects.ts
+++ src/locales/pt-BR/page/projects.ts
@@ -5,13 +5,13 @@ export const ProjectsPage: PageFullType<ProjectsPageStaticLocalesSource> = {
    mainProps: {
       header: {
          title: 'Projetos',
-         description: 'Uma coleção dos meus projetos',
+         description: 'Alguns dos meus projetos relacionados a dev. software',
          backgroundAlt: "Ilustração da página de projetos",
       }
    },
    content: {
       mainParagraphTitle: 'git checkout projects',
-      mainParagraph: "Como dito, tenho profundo interesse pela área e me invisto nela, não só conceitualmente, mas também com projetos práticos.\n\n Listar todos os meus projetos aqui, não combinaria corretamente com esse portfólio, então desenvolverei um arquivo com todos meus projetos.\n\nMas, aqui você poderá consultar os meus favoritos. Cada projeto contem uma lista das suas principais tecnologias/frameworks/bibliotecas/etc, seu escopo, área de atuação e uma descrição, acompanhada do nome (obviamente…).",
+      mainParagraph: 'Aqui você pode descobrir parte dos meus projetos. Infelizmente, listar todos eles aqui não condiz com este portfólio. Consequentemente, desenvolverei um arquivo para armazenamento.\n\nPorém a seção abaixo ainda apresenta meus prediletos!\n\nCada card de projeto o descreve e informa seus tópicos, stack tecnológica e escopo.',
       projectsList: {
          sources: {
             github: 'Repositorio do GitHub',
