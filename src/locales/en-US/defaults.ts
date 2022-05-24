68a8b7c91b6009890d195afbc277028933536599
Vicg853 <vicg853@gmail.com>
2022-05-24 16:55:46 -0300

    Updating SEO defaults

--- src/locales/en-US/defaults.ts
+++ src/locales/en-US/defaults.ts
@@ -2,8 +2,9 @@ import type { PageDefaults } from '@custom-types/locales/pages'
 
 export const defaults: PageDefaults = {
    seo: {
-      title: 'Victor Gomez\'s Portfolio | V.G.',
+      title: 'Victor Gomez\'s Portfolio',
       description: 'Hi! I\'m Victor Gomez, a full-stack developer passionate about the whole-stack and Computer science.',
-      keywords: ['full-stack', 'developer', 'passionate', 'computer science', 'fullstack', 'backend', 'frontend']
+      keywords: ['full-stack', 'developer', 'passionate', 'computer science', 'fullstack', 'backend', 'frontend'],
+      robots: ['index', 'follow']
    }
 }
