2ae33a7c26507c067ffd5632a34503cffefc98f6
Vicg853 <vicg853@gmail.com>
2022-06-07 13:51:44 -0300

    Types fix

--- src/locales/en-US/page/error.ts
+++ src/locales/en-US/page/error.ts
@@ -1,13 +1,16 @@
+import type { PageFullType } from '../../index'
 import type { ErrorPages as ErrorPagesType } from '@c-types/locales/error-pages'
 
-export const ErrorPages: ErrorPagesType = {
-   404: {
-      mainP: 'Thats the error code my brain threw after playing around with the Regex bellow:',
-   },
-   500: {
-      mainP: 'Oops... Something went wrong on the server. Thats our fault, don\'t worry.\n My dinosaurs '
-   },
-   unknown: {
-      mainP: 'Unknown error'
+export const ErrorPages: PageFullType<ErrorPagesType> = {
+   content: {
+      404: {
+         mainP: 'Thats the error code my brain threw after playing around with the Regex bellow:',
+      },
+      500: {
+         mainP: 'Oops... Something went wrong on the server. Thats our fault, don\'t worry.\n My dinosaurs '
+      },
+      unknown: {
+         mainP: 'Unknown error'
+      }
    }
 }
--- src/locales/pt-BR/page/error.ts
+++ src/locales/pt-BR/page/error.ts
@@ -1,13 +1,16 @@
+import type { PageFullType } from '../../index'
 import type { ErrorPages as ErrorPagesType } from '@c-types/locales/error-pages'
 
-export const ErrorPages: ErrorPagesType = {
-   404: {
-      mainP: 'Pagina não encontrada'
-   },
-   500: {
-      mainP: 'Erro interno do servidor'
-   },
-   unknown: {
-      mainP: 'Erro desconhecido'
+export const ErrorPages: PageFullType<ErrorPagesType> = {
+   content: {
+      404: {
+         mainP: 'Pagina não encontrada'
+      },
+      500: {
+         mainP: 'Erro interno do servidor'
+      },
+      unknown: {
+         mainP: 'Erro desconhecido'
+      }
    }
 }  
